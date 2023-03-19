e70cd5b70c8f8b5d08769f377e295e89987b1323
Slobodan Kostic <slobodan.kostic14@gmail.com>
2023-03-19 12:56:56 +0100

    First commit with minimal UI

---
+++ App/Application_Layer/APL_Enums.cs
@@ -0,0 +1,16 @@
+﻿
+namespace APL_Enums
+{
+    public enum SimConnection
+    {
+        SIM_DISCONNECTED,
+        SIM_CONNECTED
+    }
+
+    public enum FlightStatus
+    {
+        FLIGHT_NOT_STARTED,
+        FLIGHT_STARTED,
+        FLIGHT_STOPPED
+    }
+}
---
+++ App/Application_Layer/APL_FlightStatusHandler.cs
@@ -0,0 +1,47 @@
+﻿using APL_Enums;
+
+public sealed class APL_FlightStatusHandler
+{
+    public FlightStatus flightStatus = FlightStatus.FLIGHT_NOT_STARTED;
+    public SimConnection simConnection = SimConnection.SIM_DISCONNECTED;
+
+    public APL_FlightStatusHandler()
+    {
+        flightStatus = FlightStatus.FLIGHT_NOT_STARTED;
+        simConnection = SimConnection.SIM_DISCONNECTED;
+    }
+
+    public void startFlight()
+    {
+        if (simConnection == SimConnection.SIM_CONNECTED && flightStatus != FlightStatus.FLIGHT_STARTED)
+        {
+            flightStatus = FlightStatus.FLIGHT_STARTED;
+        }
+    }
+
+    public void stopFlight()
+    {
+        if (simConnection == SimConnection.SIM_CONNECTED && flightStatus == FlightStatus.FLIGHT_STARTED)
+        {
+            flightStatus = FlightStatus.FLIGHT_STOPPED;
+        }
+    }
+
+    public void connectSim()
+    {
+        if (simConnection != SimConnection.SIM_CONNECTED)
+        {
+            simConnection = SimConnection.SIM_CONNECTED;
+            flightStatus = FlightStatus.FLIGHT_NOT_STARTED;
+        }
+    }
+
+    public void disconnectSim()
+    {
+        if (simConnection != SimConnection.SIM_DISCONNECTED)
+        {
+            simConnection = SimConnection.SIM_DISCONNECTED;
+            flightStatus = FlightStatus.FLIGHT_NOT_STARTED;
+        }
+    }
+}
